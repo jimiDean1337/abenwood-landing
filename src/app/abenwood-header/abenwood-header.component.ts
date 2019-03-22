7de3ebfa4e61f97fb4dba28bea34a6d56c8d97d5
jmbofly <flynn-31199@cotc.edu>
2019-03-22 01:30:36 -0400

    init commit

--- src/app/abenwood-header/abenwood-header.component.ts
+++ src/app/abenwood-header/abenwood-header.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable, of, BehaviorSubject } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 @Component({
   selector: 'app-abenwood-header',
   templateUrl: './abenwood-header.component.html',
   styleUrls: ['./abenwood-header.component.scss']
 })
 export class AbenwoodHeaderComponent implements OnInit {
-
-  constructor() { }
+  showMobileNav: BehaviorSubject<boolean>;
+  constructor() {
+    this.showMobileNav = new BehaviorSubject(null);
+  }
 
   ngOnInit() {
+    this.showMobileNav.next(false);
+  }
+
+  toggleMobileNav() {
+    this.showMobileNav.pipe(map(show => !show));
+    const body = document.body;
+    const overlay = '<div id="mobile-body-overly"></div>';
+    body.classList.toggle('mobile-nav-active');
+    if (this.showMobileNav.getValue()) {
+      body.append(overlay);
+    } else {
+      body.querySelector('#mobile-body-overlay').remove();
+    }
   }
 
 }
+
+
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { fromEvent, of, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import * as AOS from 'aos';
+import * as $ from 'JQuery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'abenwood-landing';
+export class AppComponent implements OnInit {
+  title = 'Abenwood Property Solutions';
+
+  ngOnInit() {
+    AOS.init({
+      useClassNames: true,
+      animatedClassName: 'animated',
+      duration: 800,
+      easing: 'ease',
+    });
+  }
 }
