1e9d371bcec83bb7c83fd53286ba1f660ac11a24
jmbofly <flynn-31199@cotc.edu>
2019-03-23 07:02:09 -0400

    init commit

--- src/app/abenwood-header/abenwood-header.component.ts
+++ src/app/abenwood-header/abenwood-header.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of, BehaviorSubject } from 'rxjs';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Observable, of, BehaviorSubject, fromEvent } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 @Component({
   selector: 'app-abenwood-header',
@@ -7,24 +7,36 @@ import { map, tap } from 'rxjs/operators';
   styleUrls: ['./abenwood-header.component.scss']
 })
 export class AbenwoodHeaderComponent implements OnInit {
+  @ViewChild('navMenu') navMenu: ElementRef<HTMLElement>;
   showMobileNav: BehaviorSubject<boolean>;
   constructor() {
-    this.showMobileNav = new BehaviorSubject(null);
+    this.showMobileNav = new BehaviorSubject(false);
   }
 
   ngOnInit() {
-    this.showMobileNav.next(false);
   }
 
   toggleMobileNav() {
-    this.showMobileNav.pipe(map(show => !show));
+    const showing = this.showMobileNav.getValue();
+    this.showMobileNav.next(!showing);
     const body = document.body;
-    const overlay = '<div id="mobile-body-overly"></div>';
+    const nav = body.querySelector('#nav-menu-container');
+    const mobile = nav.cloneNode(true);
+
+    mobile.firstChild.parentElement.setAttribute('id', 'mobile-nav');
+    const mobileMenu = mobile.firstChild;
+    mobileMenu.firstChild.parentElement.setAttribute('class', '');
+    const li = body.querySelectorAll('#mobile-nav > ul > li');
+    const clicked = fromEvent(mobileMenu.childNodes, 'click');
     body.classList.toggle('mobile-nav-active');
+    console.log('mobile nav', nav, mobile, mobileMenu);
     if (this.showMobileNav.getValue()) {
-      body.append(overlay);
+      body.append(mobile);
+      clicked.subscribe(clicker => {
+        this.toggleMobileNav();
+      })
     } else {
-      body.querySelector('#mobile-body-overlay').remove();
+      mobile.firstChild.parentElement.remove();
     }
   }
 
